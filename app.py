451803e604745521996e8f0127c6b62a42222907
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-04-08 20:09:29 +0100

    Added difflib to get matches

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import json
+from difflib import SequenceMatcher, get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+    else:
+        return "Sorry this word does not exist, please try again."
 
 word = input("Enter Word: ")
 
-print(translate(word))
+print(translate(word.lower()))
