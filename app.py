9e9c97c068d16797ffe2328dda54dac7d979c54c
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-04-08 20:53:21 +0100

    Added if loop

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        yn = input("Did you mean %s instead? If yes please type Y or if not please type N: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y": 
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "Sorry this does not match, Please check it."
+        else:
+            return "Sorry I don't understand."
     else:
         return "Sorry this word does not exist, please try again."
 
